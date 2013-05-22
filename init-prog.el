30d1e40e26f8b3730479b9c38ac14720d617599a
Zypeh <ciboy2011@gmail.com>
2013-05-23 07:34:10 +0800

    mojor changes

--- init-prog.el
+++ init-prog.el
@@ -1,17 +1,52 @@
 ;; init-prog.el
+;; configure daily programming behavior.
+;; usually affects globally (in prog-mode)
 
-(add-to-list 'load-path "~/.emacs.d/vendor")
-
-;; Rainbow delimiters
+;; Rainbow-delimiters
+(add-to-list 'load-path "~/.emacs.d/vendor/rainbow-delimiters")
 (require 'rainbow-delimiters)
-; enable this rainbow parentheses globally
-(global-rainbow-delimiters-mode)
+; rainbow parentheses everywhere (Emacs24+)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 
-;; Par Edit
-; activate it automatically
-(autoload 'paredit-mode "paredit"
-  "Minor mode for pseudo-structurally editing Lisp code." t)
-(add-hook 'emacs-lisp-mode             (lambda () (paredit-mode +1)))
-(add-hook 'lisp-mode-hook              (lambda () (paredit-mode +1)))
-(add-hook 'lisp-interaction-mode-hook  (lambda () (paredit-mode +1)))
-(add-hook 'scheme-mode-hook            (lambda () (paredit-mode +1)))
+;; Auto-complete
+(add-to-list 'load-path "~/.emacs.d/vendor/auto-complete")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/vendor/auto-complete/ac-dict")
+(ac-config-default)
+;; Auto-complete options
+; finish completation by TAB instead of RET
+(define-key ac-completing-map "\t" 'ac-complete)
+(define-key ac-completing-map "\r" 'nil)
+; select candidates with C-n/C-p only when completion menu is displayed
+(setq ac-use-menu-map t)
+(define-key ac-menu-map "\C-n" 'ac-next)
+(define-key ac-menu-map "\C-p" 'ac-previous)
+; Not to use quick helps
+(setq ac-use-quick-help nil)
+; Height limits for menu
+(setq ac-menu-height 20)
+; Colors
+(set-face-background 'ac-candidate-face "#89BDFF") ;; blue-light
+(set-face-foreground 'ac-candidate-face "#F92672") ;; magenta
+(set-face-background 'ac-selection-face "#A6E22A") ;; purple
+(set-face-foreground 'ac-selection-face "#F92672") ;; magenta
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- init.el
+++ init.el
@@ -1,10 +1,24 @@
 ;;
 ;;
-;;    My emacs config
+;;     My emacs config
+;;        by Zypeh
 ;;  
-;; 31 Jan 2013 | Zypeh.
-;;
-;;
+;; 31 Jan 2013 | Start!!
+;; 10 Feb 2013 | first commit to Github 
+;; 12 Feb 2013 | + SBCL and SLIME support
+;;               + Refined setup and configs files
+;;               + README file added
+;; 16 Feb 2013 | Email client Mu4e support (deleted!)
+;; 20 Feb 2013 | + El-get added
+;;               + paredit hooks
+;;               + Elpa added
+;;   <...snip...>
+;; 22 May 2013 | Major changes
+;;               + language specified configuration file added
+;;               + auto-complete added globally
+;;               + C language style options "k&r"
+;;               + paredit globally
+;;               
 
 ;; path allocation
 (add-to-list 'load-path "~/.emacs.d/") 
@@ -13,9 +27,18 @@
 (load "init-base")   ;; Emacs basic configurations
 (load "init-keys")   ;; Emacs keys bindings
 (load "init-gui")    ;; Emacs GUI configurations
-(load "init-mu4e")   ;; Emacs mu4e email client configurations
+;(load "init-mu4e")   ;; Emacs mu4e email client configurations
 (load "init-el-get") ;; Emacs el-get configurations
+(load "init-prog")   ;; Emacs general programming behavior
+;(load "init-media")  ;; Emacs multimedia support configurations
+
+;; Emacs language setup
+(load "init-c-lang")       ;; C/C++ languages
+(load "init-lisp-lang")    ;; Lisp support
+
+
 ;; Slime
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
 ;; Replace "sbcl" with the path to your implementation
 (setq inferior-lisp-program "sbcl")
+
