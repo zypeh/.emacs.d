fa48cfd33fc82c89404fe0f4f60efeeeb3fac900
Zypeh <zypeh.geek@gmail.com>
2014-11-02 02:40:17 +0800

    Lua Language Configuration

---
+++ init-lua-lang.el
@@ -0,0 +1,6 @@
+;; Lua languages
+
+(add-to-list 'load-path "~/.emacs.d/modules/lua-mode/")
+(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
