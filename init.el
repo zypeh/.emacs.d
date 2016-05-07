9b499b54f0622ca40ac89d77dcd1aa2fee5daaad
zypeh <zypeh.geek@gmail.com>
2016-05-07 23:45:07 +0800

    Use external benchmarking scripts, and major change on the configuration

--- init.el
+++ init.el
@@ -1,24 +1,30 @@
 ;; This is the init file that bootstrap all the configurations
 ;; which is divided into a number of other files.
 
+;; Benchmarking Emacs performance
+(add-to-list 'load-path
+	     (expand-file-name "benchmark-init" user-emacs-directory))
+(require 'benchmark-init-loaddefs)
+(benchmark-init/activate)
+
 (add-to-list 'load-path (expand-file-name "init/" user-emacs-directory))
 (add-to-list 'exec-path "/usr/local/bin")
 
 (require 'init-bechmarking)
 
 ;; Configuration
-(require 'init-base)     			; Main configuration
-(require 'init-modeline) 			; Modeline-thingy
-(require 'init-keys)     			; Keybindings
-(load "packages")                       ; MELPA
+(require 'init-base)     		  ; Main configuration
+(require 'init-modeline) 		  ; Modeline-thingy
+(require 'init-keys)     		  ; Keybindings
+(load "packages")                         ; MELPA package
 (when window-system
-  (load "theme-tomorrow-night"))        ; Theme	        
+  (load "theme-tomorrow-night"))          ; Theme	        
 
-(require 'init-prog)     			; Font's and parenthesis
-(require 'init-paredit)		        ; Parenthesis matcher
+(require 'init-prog)     		  ; Font's and parenthesis
+(require 'init-paredit)		          ; Parenthesis matcher
 (require 'init-xterm)                     ; Xterm
-(require  'init-web)                       ; HTML,css,php,etc... 
-(require  'init-c-lang)                    ; C language configurations
+(require 'init-web)                       ; HTML,css,php,etc... 
+(require 'init-c-lang)                    ; C language configurations
 (require 'init-lua-lang)                  ; Lua language configurations
 (require 'init-py-lang)                   ; Python language configurations
 (require 'init-lisp-lang)                 ; Lisp dialects configurations
@@ -30,8 +36,4 @@
 (require 'server)
 (unless (server-running-p)
   (server-start))
-
-
-(message "init completed in %.2fms"
-	 (time-subtract-millis (current-time) before-init-time))
 
--- init/init-base.el
+++ init/init-base.el
@@ -1,18 +1,41 @@
-;; init-base.el
+;;
+;; Files: init-base.el
+;; General configurations
+
+(require 'init-defuns)
 
 ;; Remove Emacs and GNU booting screen
 (setq inhibit-startup-message t)
 (setq gnus-inhibit-startup-message t)
 
+;; Set default directory
+(setq default-directory "~")
+
+;; Set home directory
+(cd (expand-file-name "~/"))
+
+;; UTF-8 encodings
+(prefer-coding-system 'utf-8)
+(set-language-environment 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+
+;; Revert buffers automatically when underlying files are changed
+(global-auto-revert-mode t)
+
+;; No bell ring
+(setq ring-bell-function (lambda()))
+
 ;; Parentheses
 (show-paren-mode t)
 (setq show-paren-style 'parentheses)
 
+;; No dialog boxes
+(setq use-dialog-box nil)
+
 ;; Syntax-highlight
 (global-font-lock-mode t)
-
-;; Return at 80th column
-(setq default-fill-column 80)
 
 ;; Pointer tracking
 (setq track-eol t)
@@ -37,7 +60,7 @@
 (setq x-select-enable-clipboard t)
 
 ;; Notify current files name
-(setq frame-title-format "Emacs*%b*")
+(setq frame-title-format "%b")
 
 ;; Mouse avoidance mode
 (setq mouse-avoidance-mode 'animate)
@@ -66,13 +89,45 @@
 (setq mouse-wheel-follow-mouse 't)
 (setq scrool-step 1)
 
-;; Global
-; (global-company-mode)
- 
-;; Reduce the time after which the company auto completion popup opens
-; (setq company-idle-delay 0.2)
+;; Automatically scroll compilation window
+(setq compilation-scroll-output 1)
+(sm/mkdir-p (sm/emacs.d "cache"))
+(sm/mkdir-p (sm/emacs.d "etc"))
+(sm/mkdir-p (sm/emacs.d "cache/backups"))
 
-;; Reduce the number of characters before company kicks in
-; (setq company-minimum-prefix-length 1)
+;; Keep backups in a separate directory
+(defun make-backup-file-name (file)
+  (concat (sm/emacs.d "cache/backups/") (file-name-nondirectory file) "~"))
+
+;; Keep autosave files in /tmp
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; Change auto-save-list directory
+(setq auto-save-list-file-prefix (sm/emacs.d "cache/auto-save-list/.saves-"))
+
+;; Disable annoying lock files
+(setq create-lockfiles nil)
+
+;; Move files to trash when deleting
+(setq delete-by-moving-to-trash t)
+
+;; Ignore case for completion
+(setq completion-ignore-case t
+      read-file-name-completion-ignore-case t)
+
+;; When copying something from outside emacs, save to kill-ring
+(setq save-interprogram-paste-before-kill t)
+
+;; GC optimisation
+;; Increase garbage collection threshold to 50MB (from 0.76MB)
+(setq gc-cons-threshold 50000000）
+
+;; Mac-port defaults
+(when (equal system-type 'darwin)
+  (setq mac-option-modifier 'meta)
+  (setq mac-command-modifier 'super)
+  (global-set-key [(super v)] 'yank)
+  (global-set-key [(super q)] 'save-buffer-kill-emacs))
 
 (provide 'init-base)
